1d5e7748519c6defac02f7270021b3ad296cdb13
Andy <andy@macbookpro.home>
2025-11-03 15:35:56 +0000

    save laest

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection'
+import FeatureSession from '../components/FeatureSession'
+import TrailersSection from '../components/TrailerSection'
 
 const Home = () => {
   return (
     <>
       <HeroSection />
+      <FeatureSession />
+      <TrailersSection />
     </>
   )
 }
